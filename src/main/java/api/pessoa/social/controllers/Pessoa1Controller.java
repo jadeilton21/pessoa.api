09c26a62e4b6489be77b617e66acac95f55e9cd2
jadeilton <jadeiltondm@gmail.com>
2024-01-08 09:23:19 -0300

    feat: create class controller Pessoa1Controller

---
+++ src/main/java/api/pessoa/social/controllers/Pessoa1Controller.java
@@ -0,0 +1,66 @@
+package api.pessoa.social.controllers;
+
+import api.pessoa.social.doMain.pessoa.*;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("pessoa")
+public class Pessoa1Controller {
+
+    @Autowired
+    private PessoaRespository respository;
+
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastrarPessoa dados, UriComponentsBuilder uriBuilder) {
+        var pessoa = new Pessoa(dados);
+        respository.save(pessoa);
+        var uri = uriBuilder.path("/pessoa/{id}").buildAndExpand(pessoa.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPessoa(pessoa));
+    }
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemPessoa>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+
+        var page = respository.findAllByAtivoTrue(paginacao).map(DadosListagemPessoa::new);
+
+
+         return ResponseEntity.ok(page);
+
+
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtulizarPessoa dados){
+            var pessoa = respository.getReferenceById(dados.id());
+            pessoa.atualizarInformacoes(dados);
+
+            return ResponseEntity.ok(new DadosDetalhamentoPessoa(pessoa));
+    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id){
+
+        var pessoa = respository.getReferenceById(id);
+        pessoa.excluir();
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var pessoa = respository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoPessoa(pessoa));
+    }
+
+
+}
