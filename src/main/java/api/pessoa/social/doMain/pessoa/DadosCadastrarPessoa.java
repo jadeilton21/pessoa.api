0a05d08cb54d2189a9fbca8bb25c31d3cbbff23e
jadeilton <jadeiltondm@gmail.com>
2024-01-08 08:31:24 -0300

    feat: update

--- src/main/java/api/pessoa/social/doMain/pessoa/DadosCadastrarPessoa.java
+++ src/main/java/api/pessoa/social/doMain/pessoa/DadosCadastrarPessoa.java
@@ -1,6 +1,6 @@
-package api.pessoa.social.pessoa;
+package api.pessoa.social.doMain.pessoa;
 
-import api.pessoa.social.endereco.DadosEndereco;
+import api.pessoa.social.doMain.endereco.DadosEndereco;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
